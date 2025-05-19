c5ccf17ad061f6007da4c8c3ccdb418100e52832
Juliusz Gonera <jgonera@gmail.com>
2025-05-19 10:26:49 -0700

    Switch to .ts config

--- prettier.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  semi: false,
-  plugins: ['prettier-plugin-embed', 'prettier-plugin-sql'],
-  // prettier-plugin-sql options
-  formatter: 'sql-formatter',
-  keywordCase: 'upper',
-  language: 'postgresql'
-}
