e93c0768bcdfad35c1c6aa775add3e57518526ba
Juliusz Gonera <jgonera@gmail.com>
2025-06-13 16:52:15 -0700

    Add proseWrap: "always"

--- prettier.config.js
+++ prettier.config.js
@@ -1,8 +1,9 @@
 export default {
   semi: false,
-  plugins: ['prettier-plugin-embed', 'prettier-plugin-sql'],
+  plugins: ["prettier-plugin-embed", "prettier-plugin-sql"],
+  proseWrap: "always",
   // prettier-plugin-sql options
-  formatter: 'sql-formatter',
-  keywordCase: 'upper',
-  language: 'postgresql'
+  formatter: "sql-formatter",
+  keywordCase: "upper",
+  language: "postgresql",
 }
